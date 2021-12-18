643a1a8590b9d59ff943e406ebc08ec804066dd4
prince chauhan <bolbam13@gmail.com>
2021-12-18 13:03:37 +0530

    working double hash

--- Hashing/doublehashing.cpp
+++ Hashing/doublehashing.cpp
@@ -1,33 +1,78 @@
-#include<bits/stdc++.h>
+#include <iostream>
 #define SIZE 10
+#define PRIME 7
+
 using namespace std;
-
-int hashOne(int key)
-{
-    return key%SIZE;
+ 
+template <class T>
+void Print(T& vec, int n, string s){
+    cout << s << ": [" << flush;
+    for (int i=0; i<n; i++){
+        cout << vec[i] << flush;
+        if (i < n-1){
+            cout << ", " << flush;
+        }
+    }
+    cout << "]" << endl;
 }
-
-int hashTwo(int key)
-{
-    return SIZE - (key % SIZE);
+ 
+int Hash(int key){
+    return key % SIZE;
 }
-
-void Insert(int H[],int key)
-{
-    int index = hashOne(key);
+ 
+int PrimeHash(int key){
+    return PRIME - (key % PRIME);
+}
+ 
+int DoubleHash(int H[], int key){
+    int idx = Hash(key);
     int i = 0;
-    while(H[(index+i*hashTwo(key))%SIZE] != 0)
+    while (H[(Hash(idx) + i * PrimeHash(idx)) % SIZE] != 0){
         i++;
-    H[(index+i*hashTwo(key))%SIZE] = key;    
+    }
+    return (idx + i * PrimeHash(idx)) % SIZE;
 }
-
-int main()
-{
-    int H[10] =  {0};
-    Insert(H,5);
-    Insert(H,25);
-    Insert(H,15);
-    Insert(H,35);
-    Insert(H,95);
-return 0;
+ 
+void Insert(int H[], int key){
+    int idx = Hash(key);
+ 
+    if (H[idx] != 0){
+        idx = DoubleHash(H, key);
+    }
+    H[idx] = key;
+}
+ 
+int Search(int H[], int key){
+    int idx = Hash(key);
+    int i = 0;
+    while (H[(Hash(idx) + i * PrimeHash(idx)) % SIZE] != key){
+        i++;
+        if (H[(Hash(idx) + i * PrimeHash(idx)) % SIZE] == 0){
+            return -1;
+        }
+    }
+    return (Hash(idx) + i * PrimeHash(idx)) % SIZE;
+}
+ 
+ 
+int main() {
+ 
+    int A[] = {5, 25, 15, 35, 95};
+    int n = sizeof(A)/sizeof(A[0]);
+    Print(A, n, " A");
+ 
+    // Hash Table
+    int HT[10] = {0};
+    for (int i=0; i<n; i++){
+        Insert(HT, A[i]);
+    }
+    Print(HT, SIZE, "HT");
+ 
+    int index = Search(HT, 25);
+    cout << "key found at: " << index << endl;
+ 
+    index = Search(HT, 35);
+    cout << "key found at: " << index << endl;
+ 
+    return 0;
 }
